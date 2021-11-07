4c8fbdc1ad909d5b98fdfa20152123cad68c9cf8
barikassa <barikassa01@gmail.com>
2021-11-07 12:03:24 +0200

    deployment

--- heroku.js
+++ heroku.js
@@ -1,14 +1,20 @@
 const express=require("express"),
-app=express(),
-PORT=8080;
-app.listen(PORT,()=>{
-    console.log(`app is liteningon:${PORT}`);
-})
+app=express();
+const dotenv=require("dotenv").config();
+const PORT=process.env.PORT;
+
+
 
 app.get("/",(req,res)=>{
     res.send("my first deployment");
 })
+app.get("*",(req,res)=>{
+    res.send("the page is not working");
+});
 
+app.listen(PORT,()=>{
+    console.log(`app is liteningon:${PORT}`);
+})
 
 
 
